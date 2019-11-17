6c26fbf2319f272cbc41597ac5d52ad4b5da9e15
Damian Reyes <reyesmezadamian@gmail.com>
2019-11-17 02:19:56 +0000

    Done.

--- lib/math.rb
+++ lib/math.rb
@@ -1,23 +1,28 @@
-def addition(num1, num2)
+$num1 = 5
+$num2 = 4
 
+def addition(num1, num2)
+  num1 + num2
 end
 
 def subtraction(num1, num2)
-
+ num1 - num2
 end
 
 def division(num1, num2)
-
+ num1 / num2
 end
 
 def multiplication(num1, num2)
-
+ num1 * num2
 end
 
 def modulo(num1, num2)
-
+ num1 % num2
 end
 
 def square_root(num)
-
+ root = Math.sqrt(81)
+ puts root
+ return root
 end
